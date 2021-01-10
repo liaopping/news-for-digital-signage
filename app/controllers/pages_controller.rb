8098c05e456e919f7c370800907277ea1e0d7644
liaopping <race.lifes.a.race@gmail.com>
2021-01-10 11:00:29 +0900

    feat(pages_controller, main.html.erb): ページをリロードするたびにニュースを10件取得し、その中からランダムで一つ表示する機能を実装
    pages_controllerでBing News Search APIを叩き、responseをハッシュ化、ハッシュ化した連想配列の中からランダムで一つ取り出し、
    responseからタイトル・概要・記事提供元・画像をインスタンス変数に格納し、 viewで受け取って表示、CSSの調整

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,41 @@
+class PagesController < ApplicationController
+  require 'net/https'
+  require 'uri'
+  require 'json'
+  require 'time'
+
+  def main
+    accessKey = "fc0a1e325d724ee4a3cb084916a9f810"
+    uri  = "https://api.bing.microsoft.com" #APIエンドポイント
+    path = "/v7.0/news?category=Business" #ニュース検索のURL
+
+    uri = URI(uri + path)
+    request = Net::HTTP::Get.new(uri)
+    request['Ocp-Apim-Subscription-Key'] = accessKey
+    @response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        http.request(request)
+    end
+    hash = JSON.parse(@response.body)
+    random_article = hash["value"].sample
+    @title = random_article["name"]
+    @description = random_article["description"]
+    # 文末が。じゃない場合に...にする処理
+    @provider_name = random_article["provider"][0]["name"]
+    # providerのimageあるなしで分岐する処理
+    if random_article["datePublished"] != nil
+      parsed_date_published_at = Time.parse(random_article["datePublished"]).in_time_zone('Tokyo')
+      @date_published_at = "#{parsed_date_published_at.month}月#{parsed_date_published_at.day}日#{parsed_date_published_at.hour}時#{parsed_date_published_at.min}分"
+    else
+      @date_published_at = ""
+    end
+    if random_article["image"] != nil
+      @image_url = random_article["image"]["thumbnail"]["contentUrl"]
+      @image_width = random_article["image"]["thumbnail"]["width"]
+      @image_height = random_article["image"]["thumbnail"]["height"]
+    else
+      @image_url = "https://1.bp.blogspot.com/-D2I7Z7-HLGU/Xlyf7OYUi8I/AAAAAAABXq4/jZ0035aDGiE5dP3WiYhlSqhhMgGy8p7zACNcBGAsYHQ/s1600/no_image_square.jpg"
+      @image_width = 250
+      @image_height = 250
+    end
+  end
+end
