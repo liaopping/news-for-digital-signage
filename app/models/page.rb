fdcb3c66b03d4f30be0798ff0bd5df556d274af3
liaopping <race.lifes.a.race@gmail.com>
2021-01-30 09:39:06 +0900

    refactor(pages_controller, page.rb): time_diff_between_current_and_latest_newsを実装
    controllerからviewへの値の受け渡し方を調整(controllerではなくviewで属性を参照するように変更), resources :pages をroutes.rbに追記,
    現在時刻と最新ニュースの時間差チェックのロジックをmodelに移動しクラスメソッドとして定義

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,11 @@
 class Page < ApplicationRecord
+  def self.time_diff_between_current_and_latest_news
+    if self.exists?
+      latest_news = self.order(date_published: "DESC").first
+      time_diff_sec = Time.now - latest_news["date_published"]
+      time_diff_hour = time_diff_sec / 3600
+    else
+      time_diff_hour = 3
+    end
+  end
 end
