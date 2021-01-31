8b6e574f9c553d75bdb4d9634ee29f2d1b8e436b
liaopping <race.lifes.a.race@gmail.com>
2021-01-31 16:13:01 +0900

    feat(pages_controller, routes.rb, show.html.erb, scheduler.rake): mainアクションをshowアクションへ変更, データ生成をHero
    - showアクション定義 - mainアクション削除 - scheduler.rake作成しpage生成の処理をmainアクションから移動

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,88 +1,4 @@
 class PagesController < ApplicationController
-  require 'net/https'
-  require 'uri'
-  require 'json'
-  require 'open-uri'
-  require 'time'
-
-  def main
-    time_diff = Page.time_diff_between_current_and_latest_news
-    # 現在の日時と最新のニュースの投稿日時の差が3時間あれば新しいニュースを取りに行く
-    if time_diff >= 3
-      # Bing News Search APIを叩く
-      uri  = "https://api.bing.microsoft.com"
-      path = "/v7.0/news/search?mkt=ja-jp&freshness=day"
-      uri = URI(uri + path)
-      request = Net::HTTP::Get.new(uri)
-      request['Ocp-Apim-Subscription-Key'] = ENV['ACCESS_KEY']
-      response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-          http.request(request)
-      end
-      # DBの中身を削除する(最新記事のみDBに保存されているようにしたい)
-      Page.destroy_all_page
-      # レスポンスの記事を全て配列に詰め、一つずつDBに保存していく
-      article_array = JSON.parse(response.body)["value"]
-      article_array.each do |article|
-        # Nokogiriで記事のHTMLを取得
-        nokogiri_url = Nokogiri::HTML(URI.open(article["url"]))
-        image_src_array = []
-        # imgタグを全て抽出し、src属性の値を全て配列に詰める
-        nokogiri_url.search('//img').each do |element_in_url|
-            if element_in_url.attributes["src"] != nil
-                image_src_array << element_in_url.attributes["src"].value
-            end
-        end
-        if image_src_array.size > 0
-          # httpsを含むURLのみ配列に詰める
-          https_image_array = image_src_array.select{ |x| x.include?("https")}
-          result_size, result_place, result_width, result_height = 0, 0, 0, 0
-          # httpsを含むURLが一つ以上あれば、画像サイズ判定の処理に進む
-          if https_image_array.size > 0
-            https_image_array.each_with_index do |elt, idx|
-              # image_size[width, height]
-              image_size = FastImage.size(elt)
-              if result_size < image_size[0] + image_size[1]
-                result_size = image_size[0] + image_size[1]
-                result_width = image_size[0]
-                result_height = image_size[1]
-                result_place = idx
-              end
-            end
-          end
-        end
-        if https_image_array != nil && https_image_array.size > 0
-          image_url = https_image_array[result_place]
-          image_width = result_width
-          image_height = result_height
-        elsif article["image"] != nil
-          image_url = article["image"]["thumbnail"]["contentUrl"]
-          image_width = article["image"]["thumbnail"]["width"]
-          image_height = article["image"]["thumbnail"]["height"]
-        else
-          image_url = "https://1.bp.blogspot.com/-D2I7Z7-HLGU/Xlyf7OYUi8I/AAAAAAABXq4/jZ0035aDGiE5dP3WiYhlSqhhMgGy8p7zACNcBGAsYHQ/s1600/no_image_square.jpg"
-          image_width = 300
-          image_height = 300
-        end
-        if article["datePublished"] != nil
-          date_published = Time.iso8601(article["datePublished"])
-        else
-          date_published = ""
-        end
-        Page.create(name: article["name"], 
-                    url: article["url"], 
-                    image_url: image_url, 
-                    image_width: image_width, 
-                    image_height: image_height, 
-                    description: article["description"], 
-                    provider_name: article["provider"][0]["name"], 
-                    date_published: date_published)
-      end
-      @random_article = Page.random_page
-    else
-      @random_article = Page.random_page
-    end
-  end
-
   def index
     # @pages = Page.all
   end
@@ -92,6 +8,6 @@ class PagesController < ApplicationController
   end
 
   def show
-    # @random_article = Page.where( 'id >= ?', rand(Page.first.id..Page.last.id) ).first
+    @random_article = Page.random_page
   end
 end
