844ce3d6903f0e4adb79701040684ed979405b36
liaopping <race.lifes.a.race@gmail.com>
2021-02-01 09:01:24 +0900

    feat(pages_controller): 特定のURLに絞ってiframeからのリクエストを許可するprivateメソッドを定義
    - allow_iframeを定義 - after_actionを定義

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,17 @@
 class PagesController < ApplicationController
+  after_action :allow_iframe, only: [:show]
   def index
     # @pages = Page.all
-  end
-
-  def create
-    #
   end
 
   def show
     @random_article = Page.random_page
   end
+
+  private
+    def allow_iframe
+        url = "https://web-digital-signage.vercel.app"
+        response.headers['X-Frame-Options'] = "ALLOW-FROM #{url}"
+        response.headers['Content-Security-Policy'] = "frame-ancestors #{url}"
+    end
 end
